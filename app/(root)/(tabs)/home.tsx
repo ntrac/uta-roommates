525f73ef81d2270605e118c4147ffd00785c13af
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2024-10-22 23:49:46 -0500

    Card on HomePage

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,6 +1,38 @@
 import { SignedIn, SignedOut, useUser, useAuth } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
-import { Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Image } from "react-native";
+import Swiper from "react-native-deck-swiper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const DUMMY_DATA = [
+  {
+    firstName: "Xavier",
+    lastName: "Rudd",
+    occupation: "Software Developer",
+    photoURL: "https://avatars.githubusercontent.com/u/24712956?v=4",
+    age: 27,
+    id: 1,
+  },
+  {
+    firstName: "Elon",
+    lastName: "Musk",
+    occupation: "Software Developer",
+    photoURL: "https://www.biography.com/.image/ar_1:1,c_fill,cs_srgb,progressive,q_auto:good,w_1200/MTc5OTkzODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
+    age: 40,
+    id: 2,
+  },
+  {
+    firstName: "Sonny",
+    lastName: "Sangha",
+    occupation: "Software Developer",
+    photoURL: "https://www.biography.com/.image/ar_1:1,c_fill,cs_srgb,progressive,q_auto:good,w_1200/MTc5OTkzODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
+    age: 21,
+    id: 3,
+  },
+];
+
 
 export default function Page() {
   const { user } = useUser();
@@ -15,9 +47,67 @@ export default function Page() {
   };
 
   return (
-    <View>
+    <SafeAreaView className="flex-1">
       <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      
+      {/* card */}
+      <View className="flex-1 -mt-6">
+      <Swiper
+        containerStyle={{ backgroundColor: 'transparent' }}
+        cards={DUMMY_DATA}
+        stackSize={5}
+        cardIndex={0}
+        animateCardOpacity
+        verticalSwipe={false}
+        onSwipedLeft={() => console.log('Swiped PASS')}
+        onSwipedRight={() => console.log('Swiped MATCH')}
+        overlayLabels={{
+          left: {
+            title: 'NOPE',
+            style: {
+              label: {
+                textAlign: 'right',
+                color: "red",
+              }
+            }
+          },
+          right: {
+            title: 'MATCH',
+            style: {
+              label: {
+                textAlign: 'left',
+                color: "green",
+              }
+            }
+          },
+        }}
+        renderCard={(card) => (
+          <View key={card.id} className="relative bg-white h-3/4 rounded-xl">
+            <Image
+              className="absolute top-0 h-full w-full rounded-xl"
+              source={{ uri: card.photoURL }}
+            />
+            <View
+              className="absolute bottom-0 bg-white w-full justify-between items-between p-4"
+              style={styles.cardShadow}
+            >
+              <Text className="text-lg font-bold">
+                {card.firstName} {card.lastName}
+              </Text>
+              <Text>{card.occupation}</Text>
+              <Text>{card.age} years</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+
+
+
+
+
+
+        {/* <Text>Hello {user?.emailAddresses[0].emailAddress}</Text> */}
         <TouchableOpacity onPress={handleSignOut} style={{ marginTop: 10 }}>
           <Text style={{ fontWeight: "bold" }}>Sign Out</Text>
         </TouchableOpacity>
@@ -31,6 +121,20 @@ export default function Page() {
           <Text>Sign Up</Text>
         </Link>
       </SignedOut>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+});
